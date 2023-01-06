46fa7f8f99ffe45c1f4dfea315462619f5f81fe7
Adriel Klein <adrielmklein@gmail.com>
2023-01-06 08:48:21 -0600

    fix title

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,12 +11,12 @@ module.exports = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
     siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitleAlt: `Adriel Klein - Software Engineer`,
+    siteHeadline: `Adriel Klein - Software Engineer`,
+    siteUrl: `https://adrielkein.com`,
+    siteDescription: `Making lives simpler by creating delightful software solutions`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `adrielklein`,
   },
   trailingSlash: `never`,
   plugins: [
